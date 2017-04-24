fcaa754e975ebb8849e8919fff5d0c702ba84e19
lr94 <lucarobbiano@gmail.com>
2017-04-24 14:46:28 +0200

    Add callback to PolitoAuthenticationProvider

--- src/net/lucarobbiano/politoftp/politoclient/PolitoAuthenticationProvider.java
+++ src/net/lucarobbiano/politoftp/politoclient/PolitoAuthenticationProvider.java
@@ -1,22 +1,38 @@
 package net.lucarobbiano.politoftp.politoclient;
 
+import java.util.function.*;
+
 import net.lucarobbiano.politoftp.ftp.*;
 
 public class PolitoAuthenticationProvider implements FtpAuthenticationProvider {
 
+    private Consumer<PolitoClient> afterLogin;
+
     @Override
     public boolean login(String username, String password, FtpServer ftpServer) {
+        boolean valToRet;
+
+        PolitoClient pClient = null;
         try {
-            PolitoClient pClient = new PolitoClient(username, password);
+            pClient = new PolitoClient(username, password);
             pClient.login();
 
             FilesystemEntry root = pClient.getFilesystemRoot();
 
             ftpServer.setRootFilesystemEntry(root);
-            return true;
+            valToRet = true;
         } catch (Exception e) {
-            return false;
+            valToRet = false;
         }
+
+        if (afterLogin != null) {
+            afterLogin.accept(pClient);
+        }
+        return valToRet;
+    }
+
+    public void setLoginCallback(Consumer<PolitoClient> consumer) {
+        afterLogin = consumer;
     }
 
 }
